c8b8d1e50a1a38c543bce82ceef3c6efa13bdb9e
Johan GOMEZ <129684968+JohanElgift@users.noreply.github.com>
2024-11-29 06:45:31 +0100

    Add files via upload

---
+++ tesIris.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Tableau de Données Iris")
+fichier = st.file_uploader("Uploadez le fichier CSV", type="csv")
+
+if fichier:
+    df = pd.read_csv(fichier)
+  
+    st.write("### Aperçu des données")
+    st.dataframe(df)
+
+    colonnes = df.iloc[:, :8]  
+    st.write("### Graphique à barres")
+    st.bar_chart(colonnes)
+
+    st.write("### Graphique en ligne")
+    st.line_chart(colonnes)
+else:
+    st.info("Veuillez uploader un fichier CSV pour afficher les données.")
